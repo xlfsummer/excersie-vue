18e8f2e250266e00af57ba4d12192ea3bff077ac
xlfsummer <xlf-summer@outlook.com>
2018-08-01 18:37:46 +0800

    列表过渡

--- 过渡&动画/进入离开&列表过渡/script.js
+++ 过渡&动画/进入离开&列表过渡/script.js
@@ -4,7 +4,12 @@ import TransitionRunner from "./component/TransitionRunner.js";
 new Vue({
     el: ".app",
     data: {
-        singleElmentShow: true
+        mutiElTrans: "save",
+        mutiCompTrans: "fold-block",
+        listTrans: {
+            list: [1, 2, 3, 4, 5, 6],
+            next: 7
+        }
     },
     components: {
         FoldBlock,
@@ -15,5 +20,33 @@ new Vue({
     },
     methods: {
         log() { console.log.call(this, Date.now(), ...arguments); },
+        velocityEnter(el, done) {
+            el.velocity({
+                opacity: 1,
+                transform: "matrix(1, 0, 0, 1, 0, 0)"
+            }, {
+                easing: "ease",
+                complete: done,
+            });
+        },
+        velocityLeave(el, done) {
+            el.style.transform = "translate(0)";
+            el.velocity({
+                opacity: 0,
+                transform: "matrix(1, 1, 1, 1, 1, 1)"
+            }, {
+                easing: "ease",
+                complete: done
+            });
+        },
+        radomListIndex() {
+            return Math.floor(Math.random() * this.listTrans.list.length);
+        },
+        listAdd() {
+            this.listTrans.list.splice(this.radomListIndex(), 0, this.listTrans.next++);
+        },
+        listRemove() {
+            this.listTrans.list.splice(this.radomListIndex(), 1);
+        }
     },
 });
