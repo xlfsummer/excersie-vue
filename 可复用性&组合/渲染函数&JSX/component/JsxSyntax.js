89421a0d9bfd8e906045558de6a4d6de94914fec
xlfsummer <xlf-summer@outlook.com>
2018-08-12 11:54:06 +0800

    complete jsx excersise

--- 可复用性&组合/渲染函数&JSX/component/JsxSyntax.js
+++ 可复用性&组合/渲染函数&JSX/component/JsxSyntax.js
@@ -1,14 +1,50 @@
+import HeaderTag from "../component/HeaderTag.js";
+
 export default {
-    prop: {
-        tag: String
+    props: {
+        myTag: String
+    },
+    data() {
+        return {
+            isFocus: false
+        };
+    },
+    methods: {
+        handleClick() {
+            this.$data.isFocus = !this.$data.isFocus;
+        }
     },
     render(h) {
         return h(
             "div",
             {
-                attrs: { title: this.tag }
+                style: {
+                    outline: this.$data.isFocus ? "1px red solid" : ""
+                },
+                attrs: { title: this.myTag,
+                    "data-title": this.myTag
+                },
+                domProps: {
+                    "_custom_dom_attrs": "Hello world"
+                },
+                on: {
+                    "click": this.handleClick
+                }
             },
-            ["Introducuing JSX"]
+            [h(
+                HeaderTag,
+                {
+                    ref: "header",
+                    attrs: { level: 4 }
+                },
+                ["Introducuing JSX"]
+            ), this.$scopedSlots.scopedSlotName(this.myTag), h("br"), this.$slots.default]
         );
+    },
+    mounted() {
+        console.group("jsx demo");
+        console.log("自定义 dom 属性:", this.$el._custom_dom_attrs);
+        console.log("ref", this.$refs.header.$el);
+        console.groupEnd("jsx demo");
     }
 };
--- 可复用性&组合/渲染函数&JSX/src/JsxSyntax.jsx
+++ 可复用性&组合/渲染函数&JSX/src/JsxSyntax.jsx
@@ -1,10 +1,43 @@
+import HeaderTag from "../component/HeaderTag.js";
+
 export default {
-    prop: {
-        tag: String
+    props: {
+        myTag: String
+    },
+    data(){
+        return {
+            isFocus: false,
+        };
+    },
+    methods: {
+        handleClick(){
+            this.$data.isFocus = !this.$data.isFocus;
+        },
     },
     render(h){
         return (
-            <div title={this.tag}>Introducuing JSX</div>
+            <div
+                style={{
+                    outline: this.$data.isFocus ? "1px red solid" : ""
+                }}
+                title={this.myTag}
+                data-title={this.myTag}
+                domProps_custom_dom_attrs={"Hello world"}
+                onClick={this.handleClick}>
+                {/* level 被编译为 attr 中的属性, 但仍然作为 props 生效*/}
+                <HeaderTag
+                    ref="header"
+                    level={4}>Introducuing JSX</HeaderTag>
+                {this.$scopedSlots.scopedSlotName(this.myTag)}
+                <br/>
+                {this.$slots.default}
+            </div>
         );
+    },
+    mounted(){
+        console.group("jsx demo");
+        console.log("自定义 dom 属性:", this.$el._custom_dom_attrs);
+        console.log("ref", this.$refs.header.$el);
+        console.groupEnd("jsx demo");
     }
 };
