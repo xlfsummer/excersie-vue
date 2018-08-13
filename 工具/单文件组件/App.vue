f4c093cf0c07ce6b601973981369107bcedb3099
xlfsummer <xlf-summer@outlook.com>
2018-08-13 23:43:09 +0800

    complete the basic framework and config to Single Page Component

---
+++ 工具/单文件组件/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg: "Hello world"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        font-family: 'Times New Roman', Times, serif;
+    }
+</style>
---
+++ 工具/单文件组件/webpack.config.js
@@ -0,0 +1,66 @@
+let path = require("path");
+// webpack4 不再支持 ExtractTextPlugin
+// let ExtractTextPlugin = require("extract-text-webpack-plugin");
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let VueLoaderPlugin = require("vue-loader").VueLoaderPlugin;
+const fs = require("fs");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "script.js"),
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+                options: {
+                    compiler: require("vue-template-compiler"),
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            // https://github.com/webpack-contrib/mini-css-extract-plugin
+                        }
+                    },
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "style.css",
+            // chunkFilename: "[id].css"
+        }),
+        // .eslintignore config can't resolve chinese char folder name
+        new (class AddEslintIgnorePlugin{
+            apply(compiler){
+                compiler.hooks.done.tap("AddEslintIgnorePlugin", (stats)=>{
+                    let output = path.resolve(
+                        stats.compilation.outputOptions.path,
+                        stats.compilation.outputOptions.filename
+                    );
+                    fs.writeFileSync(
+                        output,
+                        "/* eslint-disable */\r\n"
+                        + fs.readFileSync(output, "utf8"),
+                        "utf8"
+                    );
+                });
+            }
+        })()
+    ]
+};
