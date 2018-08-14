b62d1979d88032d47c218d1f792ff6fb6c90cba4
xlfsummer <xlf-summer@outlook.com>
2018-08-15 01:10:44 +0800

    add basic file, add *.ts

---
+++ 工具/TypeScript支持/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <input type="colot" v-model="color"/>
+        <div>{{HexColor}}</div>
+        <div>{{RgbColor}}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data(){
+        return {
+            msg: "Hello world",
+            color: "#000"
+        }
+    },
+    methods: {
+
+    }
+});
+</script>
+
+<style scoped>
+    h1{
+        font-family: 'Times New Roman', Times, serif;
+    }
+</style>
