e8b41da6cb37cf84b1489a29dcd5d0afc5d99213
xlfsummer <xlf-summer@outlook.com>
2018-08-24 15:47:59 +0800

    programmatic router

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,18 +1,24 @@
 <template>
     <main>
         <header>
-            <h1>Hello world</h1>
-            <p>{{msg}}</p>
+            <h1>Vue-Router Demo</h1>
             <nav>
+                <router-link to="/">Home</router-link>
                 <router-link to="/about">About</router-link>
-                <router-link to="/news">News</router-link>
-                <router-link to="/article-list">Article List</router-link>
+                <router-link to="/news">News (Nest Route)</router-link>
+                <router-link to="/article">Article List (Param Match)</router-link>
+
+                <input v-model="routeInput" @keypress.enter="go"/><button @click="go">Go</button>
+                <button @click="goPrev">&lt;</button>
+                <button @click="goNext">&gt;</button>
             </nav>
+            <hr/>
         </header>
+
         <!-- 路由出口 -->
         <!-- 路由匹配到的组件将渲染在这里 -->
-        <router-view>
-        </router-view>
+        <router-view></router-view>
+
         <footer>
             <hr/>
             copyright
@@ -24,9 +30,20 @@
 export default {
     data(){
         return {
-            msg: "xxx"
+            routeInput: ""
         }
     },
+    methods: {
+        go(){
+            this.$router.push(this.$data.routeInput)
+        },
+        goPrev(){
+            this.$router.go(-1);
+        },
+        goNext(){
+            this.$router.go(1);
+        }
+    }
 }
 </script>
 
@@ -38,10 +55,11 @@ export default {
         display: grid;
         align-content: stretch;
         min-height: 100%;
-        grid-template: 
-           "header" auto 
-           "main"   1fr
-           "footer" auto
+        grid-template:
+           "header"     auto
+           "main"       1fr
+           "footer"     auto
+
            /1fr;
     }
 </style>
--- vue-router/src/route.js
+++ vue-router/src/route.js
@@ -2,22 +2,50 @@ import PageAbout from "./components/PageAbout.vue";
 import PageNews from "./components/PageNews.vue";
 import PageArticle from "./components/PageArticle.vue";
 import PageArticleList from "./components/PageArticleList.vue";
+import PageDefault from "./components/PageDefault.vue";
+import PageNewsHome from "./components/PageNewsHome.vue";
+import PageNewsEdit from "./components/PageNewsEdit.vue";
+import PageNewsView from "./components/PageNewsView.vue";
 
 export default [
+    {
+        path: "/",
+        component: PageDefault
+    },
     {
         path: "/about",
         component: PageAbout
     },
     {
         path: "/news",
-        component: PageNews
+        component: PageNews,
+
+        // 定义嵌套路由
+        children: [
+            {
+                //match /news
+                path: "",
+                component: PageNewsHome
+            },
+            {
+                //match /news/edit
+                path: "edit",
+                component: PageNewsEdit
+            },
+            {
+                //match /news/view
+                path: "/news/view",
+                component: PageNewsView
+            }
+        ]
     },
     {
-        path: "/article/:id",
+        //高级参数匹配: id 满足规则 \d+
+        path: "/article/:id(\\d+)",
         component: PageArticle
     },
     {
-        path: "/article-list",
+        path: "/article",
         component: PageArticleList
     }
 ];
